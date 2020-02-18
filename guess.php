169259fcbec5f6e1b51b839b0cc71fb2128076b3
Joe <joe@joebailey.xyz>
2020-02-18 12:16:14 +0000

    Prevented XSS

--- guess.php
+++ guess.php
@@ -1,4 +1,11 @@
 <?php 
+    session_set_cookie_params(
+        0,                 // Lifetime -- 0 means erase when browser closes
+        '/',               // Which paths are these cookies relevant?
+        'hangman.joebailey.xyz', // Only expose this to which domain?
+        true,              // Only send over the network when TLS is used
+        true               // Don't expose to Javascript
+    );
     //Start Session
     session_start();
     //If game restarted or accessed directly
@@ -9,6 +16,9 @@
         //Redirect to homepage
         header("Location: index.php");
         exit;
+    }
+    function noHTML($input, $encoding = 'UTF-8') {
+        return htmlentities($input, ENT_QUOTES | ENT_HTML5, $encoding);
     }
 ?>
 <!DOCTYPE html>
@@ -25,9 +35,9 @@
         //If word has just been entered on homepage
         if ($_POST["word"]) {
             //Create Word Session Variable with lowercase array of word
-            $_SESSION['word'] = array_unique(str_split(strtolower($_POST["word"])));
+            $_SESSION['word'] = array_unique(str_split(strtolower(noHTML($_POST["word"]))));
             //Create Word Session Variable with duplicates
-            $_SESSION['original_word'] = str_split(strtolower($_POST["word"]));
+            $_SESSION['original_word'] = str_split(strtolower(noHTML($_POST["word"])));
             //Create Letters Session Variable with array length
             $_SESSION['letters'] = sizeof($_SESSION['word']);
             //Set the game in motion
@@ -71,7 +81,7 @@
             $word = $_SESSION['word'];
             $turn = $_SESSION['turn'];
             //Get guess
-            $guess = strtolower($_POST["guess"]);
+            $guess = strtolower(noHTML($_POST["guess"]));
 
             //If there have been previous guesses
             if ($_SESSION['guesses']) {
